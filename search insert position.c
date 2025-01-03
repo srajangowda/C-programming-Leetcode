9b174655830f68f1653e8c8c32f19cc5512973a5
Srajan Gowda <srajangowda95@gmail.com>
2025-01-03 22:07:50 +0530

    Create search insert position.c

---
+++ search insert position.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target){
+    int i=0;
+    int j=numsSize-1;
+    int m;
+    while (i<=j) {
+        m = (i+j)/2;
+        if (nums[m]==target) {
+            return m;
+        } else if (nums[m]<target) {
+            i = m+1;
+        } else {
+            j = m-1;
+        }
+    }
+    return i;
+}
